b81154dcb692d544b5afdbeb1b6d3661437c7a28
johntvale <johntvale@gmail.com>
2021-07-04 17:10:49 -0400

    Requisito 8 resolvido

--- src/zoo.js
+++ src/zoo.js
@@ -1,7 +1,8 @@
-const { species } = require('./data');
+const { species, prices } = require('./data');
 const data = require('./data');
 
 const { employees } = data;
+const { Adult, Senior, Child } = prices;
 
 function getSpeciesByIds(...ids) {
   // Requisito 1
@@ -61,7 +62,6 @@ function countAnimals(recSpecie) { // alterei 'species' por 'recSpecie' para cor
   // Requisito 7
   const resultObject = {};
 
-  // sem parâmetros
   const resultNoName = species.map((eachSpecie) => eachSpecie.name);
   const resultQtyNoName = species.map((eachSpecie) => eachSpecie.residents.length);
 
@@ -73,12 +73,60 @@ function countAnimals(recSpecie) { // alterei 'species' por 'recSpecie' para cor
     return resultObject;
   }
 
-  // com parâmetros
   return species.find((foundSpecie) => foundSpecie.name === recSpecie).residents.length;
 }
 
+function calculateLength1(entrants) { // Teste 1 Requisito 8
+  const keyName = Object.keys(entrants)[0];
+  const value = Object.values(entrants)[0];
+
+  if (keyName === 'Adult') {
+    const calculatedEntry1 = value * Adult * 100;
+    return calculatedEntry1 / 100;
+  } if (keyName === 'Senior') {
+    const calculatedEntry2 = value * Senior * 100;
+    return calculatedEntry2 / 100;
+  } if (keyName === 'Child') {
+    const calculatedEntry3 = value * Child * 100;
+    return calculatedEntry3 / 100;
+  }
+}
+
+function calculateLength2(entrants, arrPricesOrdered) { // Teste 2 Requisito 8
+  const arrPricesChildSenior = arrPricesOrdered.slice(1);
+  const valuesCS = Object.values(entrants);
+  const calculatedEntry4 = valuesCS
+    .map((valueCS, index) => valueCS * arrPricesChildSenior[index] * 100)
+    .reduce((accumulator = 0, actualValue = calculatedEntry4[0]) =>
+      accumulator + actualValue) / 100;
+  return calculatedEntry4;
+}
+
+function calculateLength3(entrants, arrPricesOrdered) {
+  const recValues = Object.values(entrants);
+  const calculatedEntry5 = recValues
+    .map((recValue, index) => recValue * arrPricesOrdered[index] * 100)
+    .reduce((accumulator = 0, actualValue = calculatedEntry5[0]) =>
+      accumulator + actualValue) / 100;
+  return calculatedEntry5;
+}
+
 function calculateEntry(entrants) {
-  // seu código aqui
+  // Requisito 8
+  if (!entrants) return 0;
+  if (Object.keys(entrants).length === 0) return 0;
+
+  const arrPricesOrdered = [Adult, Child, Senior];
+
+  if (Object.values(entrants).length === 1) {
+    return calculateLength1(entrants);
+  }
+
+  if (Object.values(entrants).length === 2) {
+    return calculateLength2(entrants, arrPricesOrdered);
+  }
+
+  return calculateLength3(entrants, arrPricesOrdered);
 }
 
 function getAnimalMap(options) {
